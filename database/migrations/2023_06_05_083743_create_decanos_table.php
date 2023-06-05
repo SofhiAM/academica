b865c9003fa1fcb998e079f4a2be630137c8c4fe
Jenny Sofia Moreno <jennysmr2014@gmail.com>
2023-06-05 04:07:06 -0500

    Migración de las BD

---
+++ database/migrations/2023_06_05_083743_create_decanos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('decanos', function (Blueprint $table) {
+            $table->char('coddecano',2);
+            $table->char('nomdecano',40);
+            $table->char('facultad',2);
+
+            $table->primary('coddecano');
+            $table->foreign('facultad')
+                ->references('codfacultad')->on('facultades');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('decanos');
+    }
+};
