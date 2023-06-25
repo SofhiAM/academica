a7f59edc8cbfc57f066246666127d7c0292a804b
HernanMoncayo <hedaco.mc@udenar.edu.co>
2023-06-24 21:30:20 -0500

    Listado profesores y estudiantes

--- resources/views/estudiantes/listado.blade.php
+++ resources/views/estudiantes/listado.blade.php
@@ -3,13 +3,62 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Estudiantes</h1>
+    <h1>Facultades</h1>
 
 
 @stop
 
 @section('content')
-    <p>Listado de Estudiantes</p>
+<p>Listado de Estudiantes</p>
+
+<a href="/estudiantes/registro" class="btn btn-success">Adicionar Estudiante</a>
+<br><br>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Código</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Edad</th>
+            <th scope="col">Fecha de nacimiento</th>
+            <th scope="col">Sexo</th>
+            <th scope="col">Ciudad</th>
+            <th scope="col">Barrio</th>
+            <th scope="col">Programa</th>
+            <th scope="col">Opciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $contador=1
+        @endphp
+        @foreach ($estudiante as $f)
+        <tr>
+            <th scope="row">
+                {{$contador}}
+            </th>
+            <td>{{$f->codestudiante}}</td>
+            <td>{{$f->nomestudiante}}</td>
+            <td>{{$f->edadestudiante}}</td>
+            <td>{{$f->fechestudiante}}</td>
+            <td>{{$f->sexestudiante}}</td>
+            <td>{{$f->ciudad}}</td>
+            <td>{{$f->barrio}}</td>
+            <td>{{$f->programa}}</td>
+            <td>
+                <a href="{{route('eliminaEst', $f->codestudiante)}}" class="btn btn-primary">Editar</a>
+                <a href="{{route('eliminaEst', $f->codestudiante)}}" class="btn btn-danger">Eliminar</a>
+            </td>
+        </tr>
+        <tr>
+        @php
+            $contador = $contador+1
+        @endphp
+        @endforeach
+        
+        </tr>
+    </tbody>
+    </table>
 
 @stop
 
--- resources/views/profesores/listado.blade.php
+++ resources/views/profesores/listado.blade.php
@@ -3,13 +3,52 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Profesores</h1>
+    <h1>Facultades</h1>
 
 
 @stop
 
 @section('content')
-    <p>Listado de Profesores</p>
+<p>Listado de Profesores</p>
+
+<a href="/profesores/registro" class="btn btn-success">Adicionar Profesor</a>
+<br><br>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Código</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Categoria</th>
+            <th scope="col">Opciones</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php
+            $contador=1
+        @endphp
+        @foreach ($profesores as $f)
+        <tr>
+            <th scope="row">
+                {{$contador}}
+            </th>
+            <td>{{$f->codprofesor}}</td>
+            <td>{{$f->nomprofesor}}</td>
+            <td>{{$f->catprofesor}}</td>
+            <td>
+                <a href="{{route('eliminaFac', $f->codprofesor)}}" class="btn btn-primary">Editar</a>
+                <a href="{{route('eliminaFac', $f->codprofesor)}}" class="btn btn-danger">Eliminar</a>
+            </td>
+        </tr>
+        <tr>
+        @php
+            $contador = $contador+1
+        @endphp
+        @endforeach
+        
+        </tr>
+    </tbody>
+    </table>
 
 @stop
 
